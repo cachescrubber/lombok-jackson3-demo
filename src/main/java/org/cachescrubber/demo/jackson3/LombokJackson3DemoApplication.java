7b7122a5038133b4fe3fca5ad0d8335143a57db5
Lars Uffmann <lars.uffmann@gmail.com>
2025-09-20 13:43:24 +0200

    first commit

---
+++ src/main/java/org/cachescrubber/demo/jackson3/LombokJackson3DemoApplication.java
@@ -0,0 +1,13 @@
+package org.cachescrubber.demo.jackson3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LombokJackson3DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LombokJackson3DemoApplication.class, args);
+	}
+
+}
